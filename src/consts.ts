5371a7b65729f31af251176a856062b7ae091310
Matthew Meszaros <meszmatew@gmail.com>
2026-02-21 16:03:05 +0000

    refactor: move Kit credentials from env to consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,9 @@
 export const SITE_NAME = 'memctl';
 export const SITE_TAGLINE = 'Open source memory infrastructure for AI coding agents';
 
+export const KIT_FORM_ID = '';
+export const KIT_API_KEY = '';
+
 export const SOCIAL_LINKS = {
   github: 'https://github.com/memctl',
   twitter: 'https://x.com/memctl',
