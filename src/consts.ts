bbf7b1a5e732ac6d7b190a85f014b018952e668a
Matthew Meszaros <meszmatew@gmail.com>
2026-02-21 13:07:47 +0000

    Initial Commit

---
+++ src/consts.ts
@@ -0,0 +1,8 @@
+export const SITE_NAME = 'memctl';
+export const SITE_TAGLINE = 'Open source memory infrastructure for AI coding agents';
+
+export const SOCIAL_LINKS = {
+  github: 'https://github.com/memctl',
+  twitter: 'https://x.com/memctl',
+  discord: 'https://discord.gg/rtt4JTFPdV',
+};
